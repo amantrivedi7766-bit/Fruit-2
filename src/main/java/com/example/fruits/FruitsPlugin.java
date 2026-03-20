9238d6590fe4ac3201553a9a1fcdd3134d5488a3
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-20 19:06:02 +0530

    Update FruitsPlugin.java

--- src/main/java/com/example/fruits/FruitsPlugin.java
+++ src/main/java/com/example/fruits/FruitsPlugin.java
@@ -2,12 +2,11 @@ package com.example.fruits;
 
 import com.example.fruits.commands.FruitAdminCommand;
 import com.example.fruits.commands.FruitCommand;
-import com.example.fruits.listeners.AdminGUIListener;
-import com.example.fruits.listeners.PlayerEatListener;
-import com.example.fruits.listeners.PlayerInteractListener;
+import com.example.fruits.listeners.*;
 import com.example.fruits.models.PlayerFruitData;
 import com.example.fruits.registry.FruitRegistry;
 import com.example.fruits.utils.CooldownManager;
+import com.example.fruits.utils.GracePeriodManager;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
@@ -18,14 +17,17 @@ public class FruitsPlugin extends JavaPlugin {
     private static FruitsPlugin instance;
     private FruitRegistry fruitRegistry;
     private CooldownManager cooldownManager;
+    private GracePeriodManager gracePeriodManager;
     private final Map<UUID, PlayerFruitData> activePlayers = new HashMap<>();
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+        
         fruitRegistry = new FruitRegistry();
         cooldownManager = new CooldownManager();
+        gracePeriodManager = new GracePeriodManager();
         
         getCommand("fruit").setExecutor(new FruitCommand());
         getCommand("fruitadmin").setExecutor(new FruitAdminCommand());
@@ -33,8 +35,9 @@ public class FruitsPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerEatListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new AdminGUIListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
         
-        getLogger().info("✅ FruitsPlugin enabled! Use hotkeys to activate abilities!");
+        getLogger().info("✅ FruitsPlugin enabled with 10 powerful fruits!");
     }
 
     @Override
@@ -45,5 +48,6 @@ public class FruitsPlugin extends JavaPlugin {
     public static FruitsPlugin getInstance() { return instance; }
     public FruitRegistry getFruitRegistry() { return fruitRegistry; }
     public CooldownManager getCooldownManager() { return cooldownManager; }
+    public GracePeriodManager getGracePeriodManager() { return gracePeriodManager; }
     public Map<UUID, PlayerFruitData> getActivePlayers() { return activePlayers; }
 }
