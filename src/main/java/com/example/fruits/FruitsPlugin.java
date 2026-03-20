ceb60eadb816919abb5389859fd55c5063970514
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-20 17:35:28 +0530

    Update FruitsPlugin.java

--- src/main/java/com/example/fruits/FruitsPlugin.java
+++ src/main/java/com/example/fruits/FruitsPlugin.java
@@ -34,7 +34,7 @@ public class FruitsPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new AdminGUIListener(), this);
         
-        getLogger().info("✅ FruitsPlugin enabled with 10 unique fruits!");
+        getLogger().info("✅ FruitsPlugin enabled! Use hotkeys to activate abilities!");
     }
 
     @Override
