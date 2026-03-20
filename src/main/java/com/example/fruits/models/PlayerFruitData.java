0f11787f2fd1213a8667638eddd37eeed5969985
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-20 11:24:08 +0530

    Update PlayerFruitData.java

--- src/main/java/com/example/fruits/models/PlayerFruitData.java
+++ src/main/java/com/example/fruits/models/PlayerFruitData.java
@@ -1,1 +1,27 @@
 package com.example.fruits.models;
+
+import org.bukkit.entity.Player;
+
+public class PlayerFruitData {
+    private final Player player;
+    private Fruit fruit;
+    private int usedAbilities;
+
+    public PlayerFruitData(Player player, Fruit fruit) {
+        this.player = player;
+        this.fruit = fruit;
+        this.usedAbilities = 0;
+    }
+
+    public void incrementUsed() {
+        usedAbilities++;
+        if(usedAbilities >= 3) {
+            player.getInventory().addItem(fruit.createItem());
+            fruit = null;
+            usedAbilities = 0;
+        }
+    }
+
+    public Fruit getFruit() { return fruit; }
+    public int getUsedAbilities() { return usedAbilities; }
+}
