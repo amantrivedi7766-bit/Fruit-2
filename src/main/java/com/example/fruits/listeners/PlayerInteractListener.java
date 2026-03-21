4ed1590bd9b33861c283bdd5b3ffcaa6ab00f31f
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-03-21 19:38:51 +0530

    Update PlayerInteractListener.java

--- src/main/java/com/example/fruits/listeners/PlayerInteractListener.java
+++ src/main/java/com/example/fruits/listeners/PlayerInteractListener.java
@@ -3,6 +3,7 @@ package com.example.fruits.listeners;
 import com.example.fruits.FruitsPlugin;
 import com.example.fruits.models.Fruit;
 import com.example.fruits.models.Ability;
+import com.example.fruits.abilities.NatureAbilities;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,7 +15,7 @@ import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteractListener implements Listener {
-
+    
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
@@ -28,10 +29,51 @@ public class PlayerInteractListener implements Listener {
         
         Action action = event.getAction();
         
-        // TEMPORARILY DISABLED - No abilities yet
+        // Right click for abilities
         if((action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)) {
             event.setCancelled(true);
-            player.sendMessage("§e🍎 " + fruit.getName() + " §7- Abilities coming soon!");
+            
+            if(!player.isSneaking()) {
+                // FIRST ABILITY - VINE ATTACH (Right Click)
+                if(fruitId.equals("vine_weaver") && fruit.getAbilities().size() > 0) {
+                    Entity target = getTargetEntity(player, 15);
+                    Ability ability = fruit.getAbilities().get(0);
+                    
+                    String cooldownKey = fruitId + "_0";
+                    if(FruitsPlugin.getInstance().getCooldownManager().checkCooldown(player, cooldownKey)) {
+                        ability.getExecutor().execute(player, target);
+                        FruitsPlugin.getInstance().getCooldownManager().setCooldown(player, cooldownKey, ability.getCooldown(), ability.getName());
+                    }
+                } else {
+                    player.sendMessage("§e🍎 " + fruit.getName() + " §7- Right-click ability coming soon!");
+                }
+            } else {
+                // SECOND ABILITY - OAK HAMMER (Crouch + Right Click)
+                if(fruitId.equals("vine_weaver") && fruit.getAbilities().size() > 1) {
+                    Entity target = getTargetEntity(player, 20);
+                    Ability ability = fruit.getAbilities().get(1);
+                    
+                    String cooldownKey = fruitId + "_1";
+                    if(FruitsPlugin.getInstance().getCooldownManager().checkCooldown(player, cooldownKey)) {
+                        ability.getExecutor().execute(player, target);
+                        FruitsPlugin.getInstance().getCooldownManager().setCooldown(player, cooldownKey, ability.getCooldown(), ability.getName());
+                    }
+                } else {
+                    player.sendMessage("§e🍎 " + fruit.getName() + " §7- Crouch + Right-click ability coming soon!");
+                }
+            }
+        }
+        
+        // LEFT CLICK - LAUNCH ATTACHED PLAYER
+        if((action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK)) {
+            ItemStack item = player.getInventory().getItemInMainHand();
+            if(item != null) {
+                String id = Fruit.getFruitId(item);
+                if(id != null && id.equals("vine_weaver")) {
+                    // Try to launch any attached player
+                    NatureAbilities.handleLaunch(player);
+                }
+            }
         }
     }
     
@@ -51,6 +93,39 @@ public class PlayerInteractListener implements Listener {
         if(fruit == null) return;
         
         event.setCancelled(true);
-        player.sendMessage("§e🍎 " + fruit.getName() + " §7- Abilities coming soon!");
+        
+        // FIRST ABILITY on entity click
+        if(!player.isSneaking()) {
+            if(fruitId.equals("vine_weaver") && fruit.getAbilities().size() > 0) {
+                Ability ability = fruit.getAbilities().get(0);
+                String cooldownKey = fruitId + "_0";
+                if(FruitsPlugin.getInstance().getCooldownManager().checkCooldown(player, cooldownKey)) {
+                    ability.getExecutor().execute(player, event.getRightClicked());
+                    FruitsPlugin.getInstance().getCooldownManager().setCooldown(player, cooldownKey, ability.getCooldown(), ability.getName());
+                }
+            }
+        } else {
+            // SECOND ABILITY on entity click
+            if(fruitId.equals("vine_weaver") && fruit.getAbilities().size() > 1) {
+                Ability ability = fruit.getAbilities().get(1);
+                String cooldownKey = fruitId + "_1";
+                if(FruitsPlugin.getInstance().getCooldownManager().checkCooldown(player, cooldownKey)) {
+                    ability.getExecutor().execute(player, event.getRightClicked());
+                    FruitsPlugin.getInstance().getCooldownManager().setCooldown(player, cooldownKey, ability.getCooldown(), ability.getName());
+                }
+            }
+        }
+    }
+    
+    private Entity getTargetEntity(Player player, int range) {
+        return player.getWorld().getNearbyEntities(player.getEyeLocation(), range, range, range)
+            .stream()
+            .filter(e -> e != player && e.getLocation().distance(player.getEyeLocation()) <= range)
+            .min((e1, e2) -> {
+                double d1 = e1.getLocation().distance(player.getEyeLocation());
+                double d2 = e2.getLocation().distance(player.getEyeLocation());
+                return Double.compare(d1, d2);
+            })
+            .orElse(null);
     }
 }
